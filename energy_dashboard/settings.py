5fb8ff8c52abdf77462b370c0be2175523490459
Nigel Dokter <nigeldokter@gmail.com>
2016-04-24 16:58:47 +0000

    Added nginx and supervisor configs; Static files;

--- energy_dashboard/settings.py
+++ energy_dashboard/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = '/var/www/energy_dashboard/static/'
+STATICFILES_DIR = '/var/www/energy_dashboard/static/'
 
 LOGGING = {
     'version': 1,
